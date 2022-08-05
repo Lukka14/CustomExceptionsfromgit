b17bd1e9c74e61526854f7432796f1b60c52c4c1
Luka <cxomelidze2003@gmail.com>
2022-08-05 21:59:36 +0400

    initial committ

---
+++ src/main/java/exceptions/DivideByZeroException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class DivideByZeroException extends ArithmeticException{
+}
---
+++ src/main/java/initial/App.java
@@ -0,0 +1,39 @@
+package initial;
+
+import extra.HumanAge;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+//Create 5 custom exceptions.
+//Handle exceptions in 2 ways.
+//Use try-catch with resources.
+
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args ) throws Exception {
+        try(HumanAge humanAge=new HumanAge(1,3,4,"bc")){
+            System.out.println(humanAge);
+        }
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter first integer value: ");
+        int a=scanner.nextInt();
+        System.out.print("Enter second integer value: ");
+        int b=scanner.nextInt();
+        try {
+            System.out.print(a +"/"+ b +"="+ a / b*1.0);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e);
+            System.out.print("Enter second integer value: ");
+            b = scanner.nextInt();
+            System.out.print(a +"/"+ b +"="+ a / b*1.0);
+        }
+
+
+
+    }
+}
